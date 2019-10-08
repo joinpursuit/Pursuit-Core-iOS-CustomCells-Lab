b2edd2cd2738c27d1ebf83b02eb6bc2b999e9bcf
Michelle <Michelle.cueva@gmail.com>
2019-10-08 17:55:53 -0400

    Creates UserTableViewCell programmatically

---
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/UserTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  UserTableViewCell.swift
+//  ProgrammaticUserCells
+//
+//  Created by Michelle Cueva on 10/8/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(nameLabel)
+        //configure Label's constraints
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    private func configureConstraints() {
+           nameLabel.translatesAutoresizingMaskIntoConstraints = false
+           nameLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+           
+           nameLabel.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
+       }
+
+}
--- Project/ProgrammaticUserCells/ProgrammaticUserCells/ViewController.swift
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/ViewController.swift
@@ -2,11 +2,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var users = [User]()
+    
+     lazy var tableView: UITableView = {
+            let usersTableView = UITableView()
+            usersTableView.dataSource = self
+            usersTableView.register(UserTableViewCell.self, forCellReuseIdentifier: "userCell")
+            
+            return usersTableView
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
 
