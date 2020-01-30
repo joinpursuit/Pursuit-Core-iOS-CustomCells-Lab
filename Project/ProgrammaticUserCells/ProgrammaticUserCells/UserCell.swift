8ae9afe4ee77296ef770c7d8ac7149293b8d73fa
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-30 17:08:45 -0500

    created UserCell xib file and connected its label to usercell swift file

---
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/UserCell.swift
@@ -0,0 +1,16 @@
+//
+//  UserCellCollectionViewCell.swift
+//  ProgrammaticUserCells
+//
+//  Created by Yuliia Engman on 1/30/20.
+//  Copyright © 2020 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class UsersCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+}
